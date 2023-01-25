c2c7f854a301e80e249a5de05942627353646af3
Magdalena Rahn <116031381+MagdalenaRahn@users.noreply.github.com>
2023-01-25 17:01:18 -0600

    Delete zillow_wrangle_clustering_25jan2023.sql

--- zillow_wrangle_clustering_25jan2023.sql
+++
@@ -1,6 +0,0 @@
-
-select *, distinct prop.id
-from properties_2017 as prop
-join predictions_2017 USING(parcelid)
-join propertylandusetype USING(propertylandusetypeid)
-where propertylandusetypeid = 261; 
